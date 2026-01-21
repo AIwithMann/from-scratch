c22f44c5c4b46a942df84e437ecdd0a954feb3ec
Mann Makwana <aiwithmann@gmail.com>
2026-01-21 08:23:37 +0530

    Implement Lasso and Ridge regularization in LinearRegression
    Added support for Lasso and Ridge regularization in the LinearRegression class. Updated the constructor and loss function to handle regularization parameters.

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,9 +1,17 @@
 import numpy as np
 
 class LinearRegression:
-    def __init__(self,numFeatures:int):
+    def __init__(self,numFeatures:int, ridge_:bool = False, lasso_:bool = False, lambda_:float  = 0.0):
         self.W = np.zeros(shape = (numFeatures), dtype = np.float64)
         self.b = 0.0
+        self.ridge = ridge_
+        self.lasso = lasso_
+        self.lmbda = lambda_
+        if (self.ridge or self.lasso) and self.lmbda <= 0.0:
+            raise Exception("lambda must be > 0 when regularization is enabled")
+        if(not self.ridge and not self.lasso and self.lmbda !=0.0):
+            raise Exception("lambda given but no regularization enabled");
+        
 
     def predict(self,X:np.ndarray):
         assert X[0].size == self.W.size
@@ -13,7 +21,12 @@ class LinearRegression:
     def loss(self,y:np.ndarray, y_hat:np.ndarray):
         assert y.shape == y_hat.shape
         mse  = np.sum((y_hat - y) ** 2) /y.size 
-        return mse 
+        if self.ridge:
+            mse += self.lmbda * np.sqrt(self.W ** 2)
+        if self.lasso:
+            mse += self.lmbda * np.sum(np.abs(self.W))
+
+        return mse
 
     def train(self,X:np.ndarray, y:np.ndarray, num_epochs:int, lr:float):
         assert X[0].size == self.W.size
@@ -24,7 +37,11 @@ class LinearRegression:
             e = y_hat - y 
             dW = (2.0/X.shape[0]) * X.T @ e
             db = (2.0/X.shape[0]) * e.sum()
-
+            
+            if self.ridge:
+                dW += 2.0 * self.lmbda * self.W 
+            if self.lasso:
+                dW += self.lmbda * np.sign(self.W)
             self.W -= lr * dW 
             self.b -= lr * db 
 
@@ -33,7 +50,7 @@ class LinearRegression:
 X = np.array([[1,2],[2,1],[3,0],[0,3],[4,1],[2,2],[5,0],[0,4]])
 y = np.array([13,12,11,14,16,15,15,17])
 
-model = LinearRegression(2)
+model = LinearRegression(2, False, True, 0.01)
 model.train(X,y, 10000, 0.1)
 
 y_hat = model.predict(X)
