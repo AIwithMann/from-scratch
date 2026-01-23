98e33eb93b1d2346a12b7a696954939374ecc1d5
Mann Makwana <aiwithmann@gmail.com>
2026-01-23 22:10:04 +0530

    Enhance LogisticRegression with regularization options
    Updated the LogisticRegression class to include options for ridge and lasso regularization. Added checks for lambda parameter when regularization is enabled.

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -1,14 +1,21 @@
 import numpy as np
 
 class LogisticRegression:
-    def __init__(self, num_features:int):
-        self.w = np.zeros(shape = (num_features))
-        self.b = 0
+    def __init__(self,numFeatures:int, ridge_:bool = False, lasso_:bool = False, lambda_:float  = 0.0):
+        self.W = np.zeros(shape = (numFeatures), dtype = np.float64)
+        self.b = 0.0
+        self.ridge = ridge_
+        self.lasso = lasso_
+        self.lmbda = lambda_
+        if (self.ridge or self.lasso) and self.lmbda <= 0.0:
+            raise Exception("lambda must be > 0 when regularization is enabled")
+        if(not self.ridge and not self.lasso and self.lmbda !=0.0):
+            raise Exception("lambda given but no regularization enabled"); 
         
     def predict(self,X:np.ndarray):
-        assert X.shape[1] == self.w.size
+        assert X.shape[1] == self.W.size
 
-        z = X @ self.w + self.b
+        z = X @ self.W + self.b
         y_hat = 1 / (1 + np.exp(-z))
         return y_hat
 
@@ -16,19 +23,34 @@ class LogisticRegression:
         assert y.shape == y_hat.shape
         
         loss = -np.mean(y * np.log(y_hat + 1e-15) + (1-y) * np.log(1 - y_hat + 1e-15) ) 
+
+        
+        if self.ridge:
+            loss += (self.lmbda/(2 * y.size)) * np.sum(self.W ** 2)
+
+        if self.lasso:
+            loss += (self.lmbda/y.size) * np.sum(np.abs(self.W))
+
+
         return loss
 
     def train(self, X:np.ndarray, y:np.ndarray, num_epochs:int, lr:float):
         assert X.shape[0] == y.size 
+        assert X.shape[1] == self.W.size
 
         for i in range(num_epochs):
             y_hat = self.predict(X)
             
             dz = y_hat - y
-            dw = (1/dz.size) * (X.T  @ dz)
+            dW = (1/dz.size) * (X.T  @ dz)
             db = (1/dz.size) * np.sum(dz)
+            
+            if self.ridge:
+                dW += (self.lmbda/dz.size) * self.W 
+            if self.lasso:
+                dW += (self.lmbda/dz.size) * np.sign(self.W)
 
-            self.w -= lr * dw
+            self.W -= lr * dW
             self.b -= lr * db
 
 X = np.array([
@@ -41,7 +63,7 @@ X = np.array([
 ])
 y = np.array([0,0,0,1,1,1])
 
-model = LogisticRegression(2)
+model = LogisticRegression(2, True, False, 0.01)
 model.train(X, y, 100000, 2)
 y_hat = model.predict(X)
 loss = model.loss(y, y_hat)
